cfad4bf52780da126dc382a4e6b14601be594cdb
Bridie Dibble <bridie.dibble@gdsinternational.com>
2017-08-30 15:47:53 +0100

    favicons added

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: path.resolve(__dirname, './src'),
@@ -39,6 +40,9 @@ module.exports = {
                 collapseWhitespace: true,
                 removeComments: true
             }
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: 'favicons' }
+        ])
     ],
 }
