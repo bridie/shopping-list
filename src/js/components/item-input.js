6fd5a51c8682b4de9bf2ea45fabe74714e49a20d
Bridie Dibble <bridie.dibble@gdsinternational.com>
2017-09-01 15:25:11 +0100

    coilerplate for item and item input components created

---
+++ src/js/components/item-input.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ItemInput extends Component {
+  render() {
+    return (
+      <div className="shopping-list__item-input" />
+    );
+  }
+}
+
+export default ItemInput;
